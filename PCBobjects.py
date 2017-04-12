9d61d73b26df4308b14482620760f163b636c187
marmni <marmni@interia.eu>
2017-04-12 19:30:58 +0200

    Added protection before 0 size pads - update

--- PCBobjects.py
+++ PCBobjects.py
@@ -1457,8 +1457,8 @@ class layerSilkObject(objectWire):
     # shapes
     ################
     def addRectangle(self, x1, y1, x2, y2):
-        if ([x1, y1] == [0, 0] and [x2, y2] == [0, 0]) or (x1 == x2 and y1 == y2):
-            self.spisObiektowTXT[-1]['objects'].append(['skip', "0 size point detected instead rectangle [[{0}, {1}], [{2}, {3}]]".format(x1, y1, x2, y2)])
+        if x1 == x2 and y1 == y2:
+            self.spisObiektowTXT[-1]['objects'].append(['skip', "point [{0}, {1}] detected instead rectangle".format(x1, y1)])
         else:
             self.addLine(x1, y1, x2, y1)
             self.addLine(x2, y1, x2, y2)
